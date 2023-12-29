c17bebce9ce84b97f38c368e8d8be9e5e050861f
GeorgeH <georgeharland12@gmail.com>
2023-12-29 12:46:35 +0000

    few constants changes 29/12

--- src/constants.ts
+++ src/constants.ts
@@ -41,7 +41,7 @@ export const EnemyConstants: Record<EnemyType, EnemyConfig> = {
     SPRITE_SCALE: 0.25,
     DAMAGE: 50,
     RATE: 0.015, // first around 85s
-    RATE_MULTIPLIER: 1.015,
+    RATE_MULTIPLIER: 1.005,
     SPEED_MULTIPLIER: 0.1,
     HITPOINTS: 400,
     GOLD_VALUE: 30,
@@ -60,10 +60,10 @@ export const PowerupRecord: Record<PowerupType, ItemGradeType> = {
   'Dark Blast': 'C',
   'Fire Blast': 'C',
   'Ice Spike': 'A',
-  'Poison Clouds': 'C',
+  'Poison Clouds': 'D',
   Regen: 'D',
   'Time Slow': 'S',
-  Tornado: 'D',
+  Tornado: 'C',
 };
 
 export const gradeCost: Record<ItemGradeType, number> = {
@@ -78,7 +78,7 @@ export const ARROW_BASE_SPEED = 200;
 export const ARROW_BASE_RATE = 0.2;
 export const ARROW_RATE_INCREASE = 0.3;
 
-export const CIRCLE_BASE_SPEED = 1.1;
+export const CIRCLE_BASE_SPEED = 1.2;
 export const CIRCLE_BASE_SIZE_DIVISION = 4000;
 export const CIRCLE_SPEED_INCREASE = 0.4;
 export const CIRCLE_SCALE_MULTIPLIER = 1.05;
@@ -102,7 +102,7 @@ export const ICESPIKE_BASE_SPEED = 100;
 export const ICESPIKE_LEVELUP_COOLDOWN_MULTIPLIER = 0.9;
 export const ICESPIKE_LEVELUP_POOL_INCREASE = 0.05;
 
-export const POISON_CLOUDS_BASE_AMOUNT = 2;
+export const POISON_CLOUDS_BASE_AMOUNT = 3;
 export const POISON_CLOUDS_BASE_DURATION = 2000;
 export const POISON_CLOUDS_BASE_SCALE = 0.35;
 export const POISON_CLOUDS_BASE_COOLDOWN = 5000;
@@ -117,4 +117,4 @@ export const REGEN_LEVELUP_MAXHP_INCREASE = 5;
 export const TIMESLOW_BASE_COOLDOWN = 30000;
 export const TIMESLOW_LEVELUP_COOLDOWN_MULTIPLIER = 0.9;
 
-export const TORNADO_BASE_SHAKE_AMOUNT = 4;
+export const TORNADO_BASE_SHAKE_AMOUNT = 5;
